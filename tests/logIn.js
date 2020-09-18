fa09d24ad12b67821123d21a4f99ca4a2eeb1fe1
Soumya <soumya.swaroop@gmail.com>
2020-09-18 14:43:54 +0530

    Login flow

--- tests/logIn.js
+++ tests/logIn.js
@@ -1,5 +1,5 @@
 "use strict";
-const { write, text,click,into } = require('taiko');
+const { write, text,click,below,into,textBox } = require('taiko');
 
 step("Give an option to Log In", async function () {
     await link("Log in").exists();    
@@ -9,9 +9,9 @@ step("Show the log in status for user <name>", async function (name) {
     await text("Welcome "+name+"! Not you?");    
 });
 
-step("Login as name <name> and <password>", async function (name,password) {
+step("Login as name <name> and <password>", async function (name,password1) {
     await click("Log in");
-    await write(name,into("Username or Email Address"));
-    await write(password,$("#password"));
+    await write(name,textBox(below("Username or Email Address")));
+    await write(password1,into(textBox("password")));
     await click("Log in");
 });
